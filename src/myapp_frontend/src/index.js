b129f6a815eacebc6e1d33fc0dabdc8afa2f6904
fleeB1 <fowler.lee8@gmail.com>
2022-12-23 23:41:33 +0100

    Initial commit.

---
+++ src/myapp_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { myapp_backend } from "../../declarations/myapp_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await myapp_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
